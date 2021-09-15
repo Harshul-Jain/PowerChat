1748e384b1148acb8339fb1c92da0b73c42577c9
Harshul Jain <harshuljain.mait.it@gmail.com>
2021-09-15 23:23:59 +0530

    Implemented Countdown Timer

--- app/src/main/java/com/example/powerchat/OtpActivity.kt
+++ app/src/main/java/com/example/powerchat/OtpActivity.kt
@@ -1,7 +1,9 @@
 package com.example.powerchat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.text.Spannable
 import android.text.SpannableString
 import android.text.Spanned
@@ -9,41 +11,71 @@ import android.text.TextPaint
 import android.text.method.LinkMovementMethod
 import android.text.style.ClickableSpan
 import android.view.View
+import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.activity_otp.*
 
 const val PHONE_NUMBER = "phoneNumber"
 
 class OtpActivity : AppCompatActivity() {
 
-    var phoneNumber:String? = null
+    var phoneNumber: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_otp)
         initViews()
+        showTimer(60000)
+    }
+
+    private fun showTimer(milliSecInFuture: Long) {
+        resendBtn.isEnabled = false
+        object : CountDownTimer(milliSecInFuture, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                counterTv.isVisible = true
+                counterTv.text = getString(R.string.second_remaining, millisUntilFinished / 1000)
+            }
+
+            override fun onFinish() {
+                counterTv.isVisible = false
+                resendBtn.isEnabled = true
+            }
+        }.start()
     }
 
     private fun initViews() {
-        phoneNumber=intent.getStringExtra(PHONE_NUMBER)
-        verifyTv.text=getString(R.string.verify_number,phoneNumber)
+        phoneNumber = intent.getStringExtra(PHONE_NUMBER)
+        verifyTv.text = getString(R.string.verify_number, phoneNumber)
         setSpannableString()
     }
 
     private fun setSpannableString() {
-        val span = SpannableString(getString(R.string.waiting_text,phoneNumber))
-        val clickableSpan = object : ClickableSpan(){
+        val span = SpannableString(getString(R.string.waiting_text, phoneNumber))
+        val clickableSpan = object : ClickableSpan() {
             override fun onClick(widget: View) {
                 //send back
+                showLoginActivity()
             }
 
             override fun updateDrawState(ds: TextPaint) {
                 super.updateDrawState(ds)
-                ds.isUnderlineText=false
+                ds.isUnderlineText = false
                 ds.color = ds.linkColor
             }
         }
-        span.setSpan(clickableSpan,span.length-13,span.length,Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        span.setSpan(clickableSpan, span.length - 13, span.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
         waitingTv.movementMethod = LinkMovementMethod.getInstance()
-        waitingTv.text=span
+        waitingTv.text = span
+    }
+
+    private fun showLoginActivity() {
+        startActivity(
+            Intent(this, LoginActivity::class.java)
+                .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        )
+
+    }
+
+    override fun onBackPressed() {
+
     }
 }

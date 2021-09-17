05f61fb880aaa090835c160d4b8424977b737e22
Harshul Jain <harshuljain.mait.it@gmail.com>
2021-09-17 13:48:39 +0530

    Used phone auth with Firebase

--- app/src/main/java/com/example/powerchat/OtpActivity.kt
+++ app/src/main/java/com/example/powerchat/OtpActivity.kt
@@ -1,5 +1,8 @@
 package com.example.powerchat
 
+import android.app.ProgressDialog
+import android.content.ContentValues.TAG
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -10,21 +13,47 @@ import android.text.Spanned
 import android.text.TextPaint
 import android.text.method.LinkMovementMethod
 import android.text.style.ClickableSpan
+import android.util.Log
 import android.view.View
 import androidx.core.view.isVisible
+import com.google.firebase.FirebaseException
+import com.google.firebase.FirebaseTooManyRequestsException
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthOptions
+import com.google.firebase.auth.PhoneAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_otp.*
+import java.util.concurrent.TimeUnit
+
 
 const val PHONE_NUMBER = "phoneNumber"
 
 class OtpActivity : AppCompatActivity() {
-
+    lateinit var callbacks: PhoneAuthProvider.OnVerificationStateChangedCallbacks
     var phoneNumber: String? = null
+    var mVerificationId: String? = null
+    var mResendToken: PhoneAuthProvider.ForceResendingToken? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_otp)
         initViews()
+        startVerify()
+
+    }
+
+    private fun startVerify() {
+        val options = PhoneAuthOptions.newBuilder(Firebase.auth)
+            .setPhoneNumber(phoneNumber!!)       // Phone number to verify
+            .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+            .setActivity(this)                 // Activity (for callback binding)
+            .setCallbacks(callbacks)          // OnVerificationStateChangedCallbacks
+            .build()
+        PhoneAuthProvider.verifyPhoneNumber(options)
         showTimer(60000)
+
     }
 
     private fun showTimer(milliSecInFuture: Long) {
@@ -46,6 +75,55 @@ class OtpActivity : AppCompatActivity() {
         phoneNumber = intent.getStringExtra(PHONE_NUMBER)
         verifyTv.text = getString(R.string.verify_number, phoneNumber)
         setSpannableString()
+
+        callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+            override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+                // This callback will be invoked in two situations:
+                // 1 - Instant verification. In some cases the phone number can be instantly
+                //     verified without needing to send or enter a verification code.
+                // 2 - Auto-retrieval. On some devices Google Play services can automatically
+                //     detect the incoming verification SMS and perform verification without
+                //     user action.
+                Log.d(TAG, "onVerificationCompleted:$credential")
+                val smsCode: String? = credential.smsCode
+                if (!smsCode.isNullOrBlank())
+                    sentcodeEt.setText(smsCode)
+                signInWithPhoneAuthCredential(credential)
+            }
+
+            override fun onVerificationFailed(e: FirebaseException) {
+                // This callback is invoked in an invalid request for verification is made,
+                // for instance if the the phone number format is not valid.
+                Log.w(TAG, "onVerificationFailed", e)
+
+                if (e is FirebaseAuthInvalidCredentialsException) {
+                    // Invalid request
+                } else if (e is FirebaseTooManyRequestsException) {
+                    // The SMS quota for the project has been exceeded
+                }
+
+                // Show a message and update the UI
+            }
+
+            override fun onCodeSent(
+                verificationId: String,
+                token: PhoneAuthProvider.ForceResendingToken
+            ) {
+                // The SMS verification code has been sent to the provided phone number, we
+                // now need to ask the user to enter the code and then construct a credential
+                // by combining the code with a verification ID.
+                Log.d(TAG, "onCodeSent:$verificationId")
+
+                // Save verification ID and resending token so we can use them later
+                mVerificationId = verificationId
+                mResendToken = token
+            }
+        }
+    }
+
+    private fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential) {
+
     }
 
     private fun setSpannableString() {
@@ -77,5 +155,13 @@ class OtpActivity : AppCompatActivity() {
 
     override fun onBackPressed() {
 
+    }
+}
+
+fun Context.createProgressDialog(message: String, isCancelable: Boolean): ProgressDialog {
+    return ProgressDialog(this).apply {
+        setCancelable(false)
+        setMessage(message)
+        setCanceledOnTouchOutside(false)
     }
 }
